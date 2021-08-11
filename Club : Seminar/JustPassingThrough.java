dfe4c0d776ab890b49a42c7d2ee2821dcd39db1e
HillBryan <hillbj1@appstate.edu>
2021-08-11 01:12:22 -0400

    optimization.

--- Club : Seminar/JustPassingThrough.java
+++ Club : Seminar/JustPassingThrough.java
@@ -1,15 +1,10 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class JustPassingThrough
-{
-    
-    static int count = 0;
+public class JustPassingThrough {
     
     public static void main(String[] args) {
         
         Scanner scan = new Scanner(System.in);
-        
         
         // Input
         int r = scan.nextInt();
@@ -17,11 +12,12 @@ public class JustPassingThrough
         int n = scan.nextInt();
         
         int[][] map = new int[r][c];
-        
-        //Memoization data structures
-        long[][][] memoMin = new long[r][c][n + 1];
         int[][] passes = new int[r][c];
         
+        long[][][] memoMin = new long[r][c][n + 1];
+        long min = Integer.MAX_VALUE;
+        
+        // Taking input and setting memo table to infinity
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
                 map[i][j] = scan.nextInt();
@@ -31,14 +27,14 @@ public class JustPassingThrough
             }
         }
         
+        // Preprocessing: Pass table
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
                 passes[i][j] = isPass(map, i, j);
             }
         }
         
-        long min = Integer.MAX_VALUE;
-        
+        // Top down dp
         for (int i = 0; i < r; i++) {
             if (map[i][0] != -1) {
                 min = Math.min(min, dfs(map, memoMin, passes, i, 0, n, 0));
@@ -47,14 +43,11 @@ public class JustPassingThrough
         
         if (min >= Integer.MAX_VALUE) System.out.println("impossible");
         else System.out.println(min);
-        
-        System.out.println(count);
     }
     
     public static long dfs(int[][] map, long[][][] memoMin, int[][] passMemo, int r, int c, int n, int passes) {
         
-        count++;
-        
+        // Base Cases
         if (r < 0 || r >= map.length || c < 0 || c >= map[0].length) return Integer.MAX_VALUE;
         
         if (map[r][c] == -1) return Integer.MAX_VALUE;
@@ -63,30 +56,21 @@ public class JustPassingThrough
         
         if (passes > n) return Integer.MAX_VALUE;
         
-        // Base cases
         // We get to end
         if (c == map[0].length - 1) {
-            if (passes == n) {
-                memoMin[r][c][passes] = map[r][c];
-                return map[r][c];
-            }
-            else {
-                return Integer.MAX_VALUE;
-            }
+            if (passes == n) return memoMin[r][c][passes] = map[r][c];
+            else return Integer.MAX_VALUE;
         }
         
-        //found in memo
-        if (memoMin[r][c][passes] != Integer.MAX_VALUE) {
-            return memoMin[r][c][passes];
-        }
+        // Found in memo
+        if (memoMin[r][c][passes] != Integer.MAX_VALUE) return memoMin[r][c][passes];
         
+        // Dfs step
         long east = dfs(map, memoMin, passMemo, r, c + 1, n, passes) + map[r][c];
         long nEast = dfs(map, memoMin, passMemo, r - 1, c + 1, n, passes) + map[r][c];
         long sEast = dfs(map, memoMin, passMemo, r + 1, c + 1, n, passes) + map[r][c];
         
-        memoMin[r][c][passes] = getMin(east, getMin(nEast, sEast));
-        
-        return memoMin[r][c][passes];
+        return memoMin[r][c][passes] = Math.min(east, Math.min(nEast, sEast));
     }
     
     public static int isPass(int[][] map, int r, int c) {
@@ -95,17 +79,8 @@ public class JustPassingThrough
         
         if (map[r - 1][c] == -1 || map[r + 1][c] == -1 || map[r][c - 1] == -1 || map[r][c + 1] == -1) return 0;
         
-        if (map[r][c] < map[r - 1][c] && map[r][c] < map[r + 1][c]) {
-            if (map[r][c] > map[r][c - 1] && map[r][c] > map[r][c + 1]) {
-                return 1;
-            }
-        }
+        if (map[r][c] < map[r - 1][c] && map[r][c] < map[r + 1][c] && map[r][c] > map[r][c - 1] && map[r][c] > map[r][c + 1]) return 1;
         
         return 0;
     }
-    
-    public static long getMin(long n1, long n2) {
-        return n1 >= n2 ? n2 : n1;
-    }
-
 }
